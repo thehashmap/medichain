5224429a625a9664a95b2391ac05a46c9aeb146b
thehashmap <nkmnikhil0009@gmail.com>
2023-07-13 22:41:12 +0530

    add: deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers, upgrades } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+  // Deploying the MediChain contract
+  const MediChain = await ethers.getContractFactory("MediChain");
+  console.log("Deploying MediChain contract...");
+  const mediChain = await upgrades.deployProxy(MediChain, [], {
+    initializer: "initialize",
   });
+  await mediChain.deployed();
+  console.log("MediChain contract deployed to:", mediChain.address);
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // Verifying the contract on Etherscan
+  console.log("Verifying the contract on Etherscan...");
+  await upgrades.verifyProxy(mediChain.address);
+  console.log("Contract verified on Etherscan");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
